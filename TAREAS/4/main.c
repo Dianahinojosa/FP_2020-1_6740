c3fa10b3b5053016b525bb13fd13c94d172e69c5
Dianahinojosa <diana_laura_hr@hotmail.com>
2019-10-29 23:28:53 -0600

    Modifique el archivo y le agregue comentarios al programa

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	
+	//se declara otra lista para guardar la de argv
+	//argc es el numero de argumentos
 	int lista[argc], suma=0;
+	//se declara como real ya que el promedio normalmente esta con decimales
 	float promedio;
 	
 	for(int i=1; i<argc; i++) {
 		lista[i]=atoi(argv[i]);
 	}
-	
+	//for para sumar los elementos de la lista
 	for(int i=1; i<argc; i++) {
 		suma=suma+lista[i];
 	}
-	
+	//A la suma se le divide por argc-1 para quitar el ./main 
 	promedio=(float)suma/(argc-1);
 	printf("%f\n", promedio);
 	
